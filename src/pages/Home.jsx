f3e80468f9463212060bdbe4167116d4e9f16be5
ruthwekesa <wekesaruth569@gmail.com>
2022-10-07 21:32:21 +0300

    home-page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+import Hero from '../components/ui/Hero';
+import TrendingNfts from '../components/ui/TrendingNfts/TrendingNfts';
+import SellerSection from '../components/ui/SellerSection/SellerSection';
+import CreateNfts from '../components/ui/CreateNfts/CreateNfts';
+const Home=()=> {
+  return (
+   <>
+   <Hero/>
+   <TrendingNfts/>
+   <SellerSection/>
+   <CreateNfts/>
+   </>
+  )
+}
+export default Home;
